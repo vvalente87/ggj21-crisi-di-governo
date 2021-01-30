4ceddd206542bf9e0c06ba6fe72eb7d8de915b26
Giuseppe Toto <giuseppe.toto@gmail.com>
2021-01-30 17:04:26 +0100

    creazione bozza stato idle

--- Assets/Scripts/PoliticianIdle.cs
+++ Assets/Scripts/PoliticianIdle.cs
@@ -1,25 +1,4 @@
 using UnityEngine;
 
 public class PoliticianIdle : MonoBehaviour {
-    Rigidbody2D _rigidbody;
-    [SerializeField] private Vector2 force = new Vector2(1, 0);
-    [SerializeField] private float repeatRate = 0.5f;
-
-    void Awake() {
-        _rigidbody = GetComponent<Rigidbody2D>();
-    }
-
-    private void OnEnable() {
-        if (enabled)
-            InvokeRepeating(nameof(AddForce), 0, repeatRate);
-    }
-
-    private void OnDisable() {
-        CancelInvoke(nameof(AddForce));
-    }
-
-    void AddForce() {
-        force *= -1;
-        _rigidbody.AddForce(force);
-    }
 }
